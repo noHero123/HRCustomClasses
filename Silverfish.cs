28b3232437ec60ece552de808c6b9de2dc6ed7dd
noHero <warseppl@googlemail.com>
2014-07-08 12:01:05 +0200

    -version number fix :D

--- Silverfish.cs
+++ Silverfish.cs
@@ -11,7 +11,7 @@ namespace HREngine.Bots
 
     public class Silverfish
     {
-        private int versionnumber = 70;
+        private int versionnumber = 71;
         private bool singleLog = false;
 
 
