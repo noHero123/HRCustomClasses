c9e99ddd3fbbd4e86fa06c46e0c81007d9f01346
noHero <warseppl@googlemail.com>
2014-07-30 00:36:56 +0200

    -fixed hb

--- HrtBddy/silverfish_controlHB.cs
+++ HrtBddy/silverfish_controlHB.cs
@@ -979,7 +979,7 @@ namespace SilverfishControl
             List<CardDB.cardIDEnum> ownCards = new List<CardDB.cardIDEnum>();
             List<CardDB.cardIDEnum> enemyCards = new List<CardDB.cardIDEnum>();
 
-            foreach (HREntity ent in allEntitys.Values)
+            foreach (HREntity ent in allEntitys)
             {
                 if (ent.GetTag(GAME_TAG.ZONE) == 7 && ent.GetTag(GAME_TAG.CONTROLLER) == enemycontroler) continue; // cant know enemy secrets :D
 
